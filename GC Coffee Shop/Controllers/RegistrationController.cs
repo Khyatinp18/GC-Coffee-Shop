3fc7c3187d3abc7bec3293f592a890f91e5a36ba
khyatinp18 <khyatinp18@gmail.com>
2020-02-24 22:01:24 -0500

    GC Coffee Shop

---
+++ GC Coffee Shop/Controllers/RegistrationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GC_Coffee_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GC_Coffee_Shop.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegisrtationForm()
+        {
+            return View(new RegisterUser());
+        }
+
+        public IActionResult RegistrationResult(RegisterUser registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(registerUser);
+            }
+            else
+            {
+                //the modelobject of type user did not match the 
+                //requirements we made in our class
+                return View("RegisrtationForm", registerUser);
+
+            }
+        }
+    }
+}
---
+++ GC Coffee Shop/Models/RegisterUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GC_Coffee_Shop.Models
+{
+    public class RegisterUser
+    {
+        [Required] //makes this field required for creating a Users
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Must be between 3 and 30 characters")]
+        public string FirstName { get; set; }
+
+
+        [Required] //makes this field required for creating a Users
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Must be between 2 and 30 characters")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Date of birth is required")]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+
+        [RegularExpression(@"\d{5}", ErrorMessage = "US ZipCodes only.")]
+        public int ZipCode { get; set; }
+
+     
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Please enter valid Email") ]
+        public string Email { get; set; }
+
+         //Password Length 8
+        // Require Unique Chars
+        //Require Digit
+        // Require Lower Case
+        //Require Upper Case
+        [Required]
+        [RegularExpression (@"^(?!.*([A - Za - z0 - 9])\1{1})(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", ErrorMessage = "Please enter valid password")] 
+        public string Password { get; set; }
+
+    }
+}
